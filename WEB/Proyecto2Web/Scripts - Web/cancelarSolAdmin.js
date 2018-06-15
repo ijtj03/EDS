45b8ac3f85054d09e890c7554ab0d5f5d075208e
nabetse28 <nabetse.hv.09@gmail.com>
2018-06-15 15:12:48 -0600

    Avance solicitudes

---
+++ WEB/Proyecto2Web/Scripts - Web/cancelarSolAdmin.js
@@ -0,0 +1,31 @@
+﻿var myApp = angular.module('cancelarSolAdmin', []);
+
+myApp.controller('cancelarSolAdmninCtrl', function ($scope, $http) {
+
+    $http.get('../Scripts - Web/config.json')
+        .then(function (res) {
+            $scope.config = res.data;
+            const url1 = $scope.config.MyApi;
+
+
+            $http.get(url1 + "api/Solicitud/GetAllCancelaciones?Estado=6")
+                .then(function (res) {
+                    $scope.sol = res.data;
+                    console.log(res.data);
+
+                });
+
+
+
+
+
+            $scope.modal = function (x) {
+                $scope.estudiante = x;
+                console.log($scope.estudiante);
+
+
+            }
+
+           
+        });
+});
